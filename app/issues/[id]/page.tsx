d0bdb6e0ddc27cae7b87425a4213ea64825813ca
Omaryasir <omaryasirm@gmail.com>
2024-01-18 10:02:30 +0500

    Add additional loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,8 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import delay from "delay";
 import { notFound } from "next/navigation";
-import React from "react";
 import ReactMarkDown from "react-markdown";
 
 interface Props {
@@ -13,6 +13,8 @@ const IssueDetailPage = async ({ params }: Props) => {
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
+
+  // await delay(2000);
 
   if (!issue) notFound();
 
