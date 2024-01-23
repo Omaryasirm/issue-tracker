c36191935626ed0e7a229cc6432bef03fa3b3566
Omaryasir <omaryasirm@gmail.com>
2024-01-23 16:31:55 +0500

    build an API for updating issues

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,6 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+export const issueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
 });
