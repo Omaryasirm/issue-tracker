6704276d474e83aa64b15e9be5f169b13e35aef7
Omaryasir <omaryasirm@gmail.com>
2024-04-25 01:54:01 +0500

    Remove duplicate skeletons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,31 +1,38 @@
-'use client';
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
-import { AiFillBug } from 'react-icons/ai'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
-    const currentPath = usePathname();
+  const currentPath = usePathname();
 
-    const links = [
-        { label: 'Dahsboard', href: '/' },
-        { label: "Issues", href: '/issues' },
-    ]
+  const links = [
+    { label: "Dahsboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
 
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={`${
+              link.href === currentPath && "text-zinc-900"
+            } text-zinc-500 hover:text-zinc-800 transition-colors`}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-    return (
-        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-            <Link href="/"><AiFillBug /></Link>
-            <ul className='flex space-x-6'>
-                {links.map(link =>
-                    <Link
-                        key={link.href}
-                        className={`${link.href === currentPath && 'text-zinc-900'} text-zinc-500 hover:text-zinc-800 transition-colors`}
-                        href={link.href}>{link.label}</Link>)}
-            </ul>
-        </nav>
-    )
-}
-
-export default NavBar
+export default NavBar;
