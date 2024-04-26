bb74df56c1a01b0d8b8fca5d906d4cf9f5f559fe
Omaryasir <omaryasirm@gmail.com>
2024-04-27 01:09:34 +0500

    Enhance the API to assign issues

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -2,5 +2,16 @@ import { z } from "zod";
 
 export const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  description: z.string().max(65535).min(1),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1).max(255).optional(),
+  description: z.string().min(1).max(65535).optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
 });
