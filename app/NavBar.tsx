66de976121eb9681ade5d9ab79c4ce72d895fee7
Omaryasir <omaryasirm@gmail.com>
2024-04-27 23:20:40 +0500

    Fix navbar height styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -18,7 +18,7 @@ import { Skeleton } from "./components";
 
 const NavBar = () => {
   return (
-    <nav className="border-b mb-5 px-5 py-4">
+    <nav className="border-b mb-5 px-5 py-3">
       <Container>
         <Flex justify={"between"}>
           <Flex align={"center"} gap={"3"}>
